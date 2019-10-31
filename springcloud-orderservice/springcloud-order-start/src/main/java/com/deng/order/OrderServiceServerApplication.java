2e382fc50c05479e43007a3179d12106990012fd
bigbomb <76169099@qq.com>
2019-10-31 14:10:19 +0800

    更新logback

--- springcloud-orderservice/springcloud-order-start/src/main/java/com/deng/order/OrderServiceServerApplication.java
+++ springcloud-orderservice/springcloud-order-start/src/main/java/com/deng/order/OrderServiceServerApplication.java
@@ -1,4 +1,4 @@
-package com.deng;
+package com.deng.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
